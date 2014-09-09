5e01640e7c5159312dfac5ebb0de4ba379831ceb
Bruno Gonçalves <bpvgoncalves+git@gmail.com>
2014-09-09 23:08:38 +0200

    Dataset spliting

--- w3_exploratory.R
+++ w3_exploratory.R
@@ -1,2 +1,9 @@
 library(ggplot2)
-qplot(classe, roll_belt, data=trainData, col=roll_belt)
+qplot(magnet_belt_y, accel_belt_y, data=trainClean, col=classe)
+
+summary(as.factor(trainClean$classe))
+
+axis <- names(trainClean)[7:9]
+plot(trainClean[, 8:14])
+cor(trainClean[, 8:14])
+
